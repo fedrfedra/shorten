2f0a2924b5c9b3d60b8cdabe94635c371d4a20cc
fedrfedra <vetervell@gmail.com>
2020-09-20 00:07:35 +0300

    kgs integration

--- src/main/java/com/example/shorten/repos/ShortLinkRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.shorten.repos;
-
-import com.example.shorten.model.ShortLink;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ShortLinkRepo extends MongoRepository<ShortLink, String> {
-}
